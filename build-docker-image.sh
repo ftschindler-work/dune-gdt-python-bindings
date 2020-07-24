5ac29f20c020d698695ae81e96d76ce5f4bfb0c7
Felix Schindler <felix.schindler@wwu.de>
2020-07-24 10:06:30 +0200

    [docker] updates

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache -t ftschindlerwork/dune-gdt-python-bindings -f Dockerfiles/Dockerfile Dockerfiles/
+sudo docker build --no-cache -t ftschindlerwork/dune-gdt-python-bindings -f Dockerfiles/Dockerfile Dockerfiles/
 
