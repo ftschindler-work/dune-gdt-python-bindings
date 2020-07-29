02c2823b531b51b5ab7da00c037d119006e3a830
Felix Schindler <felix.schindler@wwu.de>
2020-07-29 22:24:30 +0200

    [envs] add arch-minimal

---
+++ envs/arch-minimal/PATH.sh
@@ -0,0 +1,19 @@
+DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P )"
+export BASEDIR=$(cd ${DIR}/../.. && pwd)
+export INSTALL_PREFIX=${DIR}
+export PATH=${INSTALL_PREFIX}/bin:$PATH
+export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib64:${INSTALL_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib64/pkgconfig:${INSTALL_PREFIX}/lib/pkgconfig:${INSTALL_PREFIX}/share/pkgconfig:$PKG_CONFIG_PATH
+export CMAKE_FLAGS="-DCMAKE_DISABLE_FIND_PACKAGE_MPI=TRUE -DMINIMAL_DEBUG_LEVEL=grave -DDUNE_PYTHON_VIRTUALENV_SETUP=1 -DDUNE_PYTHON_VIRTUALENV_PATH=${INSTALL_PREFIX}/venv/dune-${OPTS}"
+export SIMDB_GIT_REPOS=$BASEDIR
+export SIMDB_PATH=${BASEDIR}/DATA
+if [ "X${OPTS}" == "X" ]; then
+    echo "You did not define OPTS (export OPTS=gcc-relwithdebinfo), not loading a virtualenv!"
+else
+    if [ -e "${DIR}/venv/dune-${OPTS}/bin/activate" ]; then
+        source "${DIR}/venv/dune-${OPTS}/bin/activate"
+    else
+        echo "WARNING: missing virtualenv for OPTS=${OPTS}, did you call setup.sh?"
+    fi
+fi
+
