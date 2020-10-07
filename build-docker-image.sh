4a118f84dd2c91b1fada79457771166406ca823c
Felix Schindler <felix.schindler@wwu.de>
2020-10-07 21:26:19 +0200

    [docker] build and run staging in staging branch

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build --no-cache -t ftschindlerwork/dune-gdt-python-bindings -f Dockerfiles/Dockerfile.arch Dockerfiles/
+sudo docker build --no-cache -t ftschindlerwork/dune-gdt-python-bindings:staging -f Dockerfiles/Dockerfile.arch Dockerfiles/
 
